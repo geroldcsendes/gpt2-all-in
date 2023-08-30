1f6d5851a65608fdbf3545e93d27ca9ddb845149
geroldcsendes <gerold.csendes@gmail.com>
2023-08-30 17:25:04 +0200

    add logging to bm

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,7 @@
 import argparse
+from datetime import datetime
+import logging
+import pathlib
 
 from transformers import GPT2Tokenizer, GPT2Model, GPT2LMHeadModel
 import torch as t
@@ -7,7 +10,15 @@ import gpt2_ai.benchmark as bm
 import gpt2_ai.benchmark.benchmark_factory as bf
 
 
+LOG_DIR = pathlib.Path(__file__).parents[0] / 'logs' / "benchmark"
+
+
 if __name__ == "__main__":
+
+    log_file = datetime.now().strftime("%Y%m%d-%H%M%S") + ".log"
+    log_file = LOG_DIR / log_file
+    logging.basicConfig(filename=log_file, level=logging.INFO)
+
 
     benchmark = bf.get_benchmark(bf.BenchmarkType.LAMBADA)
     
@@ -22,8 +33,11 @@ if __name__ == "__main__":
     tokenizer.pad_token = tokenizer.eos_token
     # tokenizer.pad_token_id = tokenizer.eos_token_id
     
+    ds = bm.lambada.LAMBADADataset(name="lambada", dev_mode=True)
+    ds.get_dataset()
 
-    benchmark = benchmark(name="lambada", model=model, tokenizer=tokenizer, device=device)
-    benchmark.get_dataset()
-    benchmark.run()
-    benchmark.debug_examples()
+    benchmark = benchmark(name="lambada", model=model, tokenizer=tokenizer,
+                          device=device, dataset=ds)
+    res = benchmark.run()
+
+    logging.info(f"Lambada: {res}")
