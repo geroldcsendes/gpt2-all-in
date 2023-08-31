409ed4e6ec90c56f11aebf54e1d8e8cf5d2fac9b
geroldcsendes <gerold.csendes@gmail.com>
2023-08-31 22:29:57 +0200

    facilitate benchmark selection

--- benchmark.py
+++ benchmark.py
@@ -14,14 +14,22 @@ LOG_DIR = pathlib.Path(__file__).parents[0] / 'logs' / "benchmark"
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task", type=str, choices=[_.value for _ in bf.BenchmarkType])
+    parser.add_argument("--dev", action="store_true", default=False)
+    args = parser.parse_args()
+
+    #benchmark = bf.get_benchmark(bf.BenchmarkType.LAMBADA)
+    benchmark, ds = bf.get_benchmark(args.task)
 
     log_file = datetime.now().strftime("%Y%m%d-%H%M%S") + ".log"
     log_file = LOG_DIR / log_file
     logging.basicConfig(filename=log_file, level=logging.INFO)
 
+    ds = ds(dev_mode=args.dev)
+    ds.get_dataset()
+    print('Ds: ', ds.dataset[:10])
 
-    benchmark = bf.get_benchmark(bf.BenchmarkType.LAMBADA)
-    
     device = 'gpu' if t.cuda.is_available() else 'cpu'
     
     model = GPT2LMHeadModel.from_pretrained('gpt2')
@@ -31,12 +39,8 @@ if __name__ == "__main__":
 
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     tokenizer.pad_token = tokenizer.eos_token
-    # tokenizer.pad_token_id = tokenizer.eos_token_id
-    
-    ds = bm.lambada.LAMBADADataset(name="lambada", dev_mode=True)
-    ds.get_dataset()
 
-    benchmark = benchmark(name="lambada", model=model, tokenizer=tokenizer,
+    benchmark = benchmark(model=model, tokenizer=tokenizer,
                           device=device, dataset=ds)
     res = benchmark.run()
 
