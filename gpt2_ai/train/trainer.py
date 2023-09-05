63850befedae2a7893f4449d3e17e1a8724f208e
geroldcsendes <gerold.csendes@gmail.com>
2023-09-05 14:55:41 +0000

    reformat and fix import trainer

--- gpt2_ai/train/trainer.py
+++ gpt2_ai/train/trainer.py
@@ -9,8 +9,8 @@ from torch.utils.data import DataLoader, Dataset
 from transformers import GPT2Tokenizer
 from torch.utils.tensorboard import SummaryWriter
 
-from gpt2_ai.config import GPT2Config
-from gpt2_ai.model import GPT2
+from gpt2_ai.train.config import GPT2Config
+from gpt2_ai.train.model import GPT2
 from gpt2_ai.util.names import get_random_name
 
 
@@ -18,7 +18,7 @@ class Trainer:
     def __init__(self, config: GPT2Config, model: GPT2,
                  train_loader: DataLoader, tokenizer: GPT2Tokenizer,
                  valid_loader=None):
-        
+
         self.config = config
         self.model = model
         self.tokenizer = tokenizer
@@ -31,7 +31,7 @@ class Trainer:
         self.ckpt_path = None
 
         self._init_log()
-    
+
     def _init_log(self):
         dt_now = datetime.now().strftime(format="%y%m%d%H")
         run_name = f"{get_random_name()}-{dt_now}"
@@ -40,7 +40,7 @@ class Trainer:
         self.ckpt_path = osp.join(self.config.ckpt_path, run_name, 'ckpt')
         if not osp.exists(self.ckpt_path):
             os.makedirs(self.ckpt_path)
-        
+
         self.run_name = run_name
 
     def train_step(self, batch: Tensor) -> Tensor:
@@ -49,11 +49,11 @@ class Trainer:
         logits = self.model(batch)[:,:-1,:]
         logits = logits.reshape(-1, self.tokenizer.vocab_size)  # [batch_size*seq_len, vocab_size]
         target = batch[:,1:].reshape(-1)  # [batch_size*seq_len]
-        
+
         loss = self.criterion(logits, target)
 
         return loss
-    
+
     @t.inference_mode()
     def valid_step(self, batch: Tensor) -> Tensor:
         self.model.eval()
@@ -61,21 +61,21 @@ class Trainer:
         logits = self.model(batch)[:,:-1,:]
         logits = logits.reshape(-1, self.tokenizer.vocab_size)  # [batch_size*seq_len, vocab_size]
         target = batch[:,1:].reshape(-1)  # [batch_size*seq_len]
-        
+
         loss = self.criterion(logits, target)
 
         return loss
-    
+
     def train(self):
-        
+
         model = self.model
         opt = self.optimizer(model.parameters(), lr=self.config.lr)
 
         step = 0
         for epoch in tqdm(range(self.config.n_epochs)):
-            
+
             for batch in self.train_loader:
-                
+
                 opt.zero_grad()
                 batch = batch['input_ids'].to(self.config.device)
 
