da1a415df98af8a641fe903762b7efe0e095f705
geroldcsendes <gerold.csendes@gmail.com>
2023-09-07 09:08:45 +0200

    trainer add ckp logic, bfloat16

--- gpt2_ai/train/trainer.py
+++ gpt2_ai/train/trainer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from functools import wraps
 import os.path as osp
 import os
 
@@ -9,17 +10,34 @@ from torch.utils.data import DataLoader, Dataset
 from transformers import GPT2Tokenizer
 from torch.utils.tensorboard import SummaryWriter
 
-from gpt2_ai.train.config import GPT2Config
+from gpt2_ai.train.config import GPT2Config, TrainerConfig
 from gpt2_ai.train.model import GPT2
 from gpt2_ai.util.names import get_random_name
 
 
+def use_bfloat16(func):
+    """
+    Decorator for using bfloat16 precision.
+    """
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if self.trainer_config.bfloat16:
+            with t.cuda.amp.autocast(dtype=t.bfloat16):
+                return func(self, *args, **kwargs)
+        else:
+            return func(self, *args, **kwargs)
+
+    return wrapper
+
+
 class Trainer:
     def __init__(self, config: GPT2Config, model: GPT2,
+                 trainer_config: TrainerConfig,
                  train_loader: DataLoader, tokenizer: GPT2Tokenizer,
                  valid_loader=None):
 
         self.config = config
+        self.trainer_config = trainer_config
         self.model = model
         self.tokenizer = tokenizer
         self.train_loader = train_loader
@@ -43,6 +61,7 @@ class Trainer:
 
         self.run_name = run_name
 
+    @use_bfloat16
     def train_step(self, batch: Tensor) -> Tensor:
         self.model.train()
 
@@ -55,6 +74,7 @@ class Trainer:
         return loss
 
     @t.inference_mode()
+    @use_bfloat16
     def valid_step(self, batch: Tensor) -> Tensor:
         self.model.eval()
 
@@ -66,39 +86,54 @@ class Trainer:
 
         return loss
 
+    def log_ckpt(self, step: int, loss: Tensor):
+
+        if step % self.config.log_interval == 0:
+            self.writer.add_scalar('loss_train', loss, step)
+
+        if step % self.config.ckpt_interval == 0:
+            t.save(self.model.state_dict(),
+                f"{self.ckpt_path}/step-{step}.pt")
+
+        if step % self.config.valid_interval == 0  and self.valid_loader is not None:
+            for valid_batch in self.valid_loader:
+                valid_batch = valid_batch['input_ids'].to(self.config.device)
+                valid_loss = self.valid_step(valid_batch)
+            self.writer.add_scalar('loss_valid', valid_loss.item(), step)
+
+        step += 1
+
+        print(f"Step: {step} | Loss: {loss.item():.3f}")
+
+        return
+
     def train(self):
 
+        train_config = self.trainer_config
         model = self.model
         opt = self.optimizer(model.parameters(), lr=self.config.lr)
 
         step = 0
-        for epoch in tqdm(range(self.config.n_epochs)):
+        for _ in tqdm(range(self.config.n_epochs)):
 
             for batch in self.train_loader:
 
-                opt.zero_grad()
                 batch = batch['input_ids'].to(self.config.device)
 
                 loss = self.train_step(batch)
                 loss.backward()
+
+                self.log_ckpt(step, loss)
+
+                # in case of gradient accumulation, check if enough steps passed
+                if train_config.gradient_accumulation_steps:
+                    if step % train_config.gradient_accumulation_steps == 0:
+                        opt.step()
+                    else:
+                        continue  # skip the rest of the loop
+
                 opt.step()
-
-                if step % self.config.log_interval == 0:
-                    self.writer.add_scalar('loss_train', loss, step)
-
-                if step % self.config.ckpt_interval == 0:
-                    t.save(self.model.state_dict(),
-                           f"{self.ckpt_path}/step-{step}.pt")
-
-                if step % self.config.valid_interval == 0  and self.valid_loader is not None:
-                    for valid_batch in self.valid_loader:
-                        valid_batch = valid_batch['input_ids'].to(self.config.device)
-                        valid_loss = self.valid_step(valid_batch)
-                    self.writer.add_scalar('loss_valid', valid_loss.item(), step)
-
-                step += 1
-
-                print(f"Step: {step} | Loss: {loss.item():.3f}")
+                opt.zero_grad()
 
         return
 
